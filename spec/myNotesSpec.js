cd6f280052dfe981a0b809d7161c80429b719bed
jameshamann <jameshamann@me.com>
2016-09-26 16:10:04 +0100

    adds ability to list all notes

--- spec/myNotesSpec.js
+++ spec/myNotesSpec.js
@@ -1,8 +1,18 @@
 function testNoteCreationAndStoring() {
   var myNotes = new MyNotes();
-  var message = "my second note"
+  var message = "my second note";
   myNotes.createNote(message);
   expect.isTrue(myNotes.notes[0].message === message);
 }
 
+function listAllNotes() {
+  var myNotes = new MyNotes();
+  var noteOne = "note one";
+  var noteTwo = "note two";
+  myNotes.createNote(noteOne);
+  myNotes.createNote(noteTwo);
+  expect.isTrue(myNotes.listNotes().length === 2);
+}
+
+listAllNotes();
 testNoteCreationAndStoring();
