70902053ed7c2d1f1f79da1fba897fa0307a8e1c
BenRoss92 <benjaminjackross@gmail.com>
2016-09-27 11:53:51 +0100

    tests creating note on click

--- Interface.js
+++ Interface.js
@@ -1,11 +1,11 @@
 var myNotes = new MyNotes();
 
-function createNoteOnClick () {
+function createNoteOnClick() {
   var x;
 
   x = document.getElementById("a_note").value;
 
-  myNotes.createNote(x);
+  console.log(myNotes.createNote(x));
   console.log(myNotes.notes);
   console.log("Note created successfully");
 }
--- spec/interfaceSpec.js
+++ spec/interfaceSpec.js
@@ -1,1 +1,9 @@
-var hello;
+function testCreateNoteOnClick() {
+  var myNotes = new MyNotesDouble();
+  var message = "hello there";
+  var Note = new NoteDouble(message);
+  
+  expect.isTrue(myNotes.createNote(message) === message);
+}
+
+testCreateNoteOnClick();
